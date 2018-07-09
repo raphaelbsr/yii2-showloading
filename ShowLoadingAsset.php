3ffb0dd9bb132c2e2d2b15829fd3b5369774eb8c
Raphael <raphael.barbosa@i9solucoesweb.com>
2018-07-08 23:10:41 -0300

    first commit

---
+++ ShowLoadingAsset.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace raphaelbsr\showloading;
+
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * @author Alexander Stepanov <student_vmk@mail.ru>
+ */
+class ShowLoadingAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/raphaelbsrmsu/yii2-showloading/assets';
+    public $css = [
+        'css/showLoading.css',
+    ];
+    public $js = [
+        'js/jquery.showLoading.min.js',
+    ];
+    public $depends = [
+        'yii\web\JqueryAsset',
+    ];
+
+    public function init()
+    {
+        parent::init();
+        //$this->js[] = YII_DEBUG ? 'js/jquery.showLoading.js' : 'js/jquery.showLoading.min.js';
+    }
+}
